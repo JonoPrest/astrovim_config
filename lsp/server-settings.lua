d96e1f9a22069bf00ffe5c53964ecc82b6ad278e
Jono Prest <jjprest@gmail.com>
2023-06-08 18:40:00 +0200

    Add yaml helper

---
+++ lsp/server-settings.lua
@@ -0,0 +1,14 @@
+return {
+  -- example for addings schemas to yamlls
+  yamlls = { -- override table for require("lspconfig").yamlls.setup({...})
+    settings = {
+      yaml = {
+        schemas = {
+          ["/Users/Jonathan/Coding/float/indexer/config-schema/config.schema.json"] = ".github/workflows/*.{yml,yaml}",
+          --         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+          --         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+        },
+      },
+    },
+  },
+}
