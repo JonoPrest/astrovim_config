4488e8d26767085756132d71f1cf00cee577f208
Jono Prest <jjprest@gmail.com>
2023-06-09 10:46:33 +0200

    Valid nvim surround config

--- plugins/init.lua
+++ plugins/init.lua
@@ -16,24 +16,15 @@ return {
         -- If TS highlights are not enabled at all, or disabled via ``disable`` prop, highlighting will fallback to default Vim syntax highlighting
         highlight = {
           enable = true,
-          disable = { "org" }, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+          disable = { "org" },                           -- Remove this to use TS highlighter for some of the highlights (Experimental)
           additional_vim_regex_highlighting = { "org" }, -- Required since TS highlighter doesn't support all syntax features (conceal)
         },
-        ensure_installed = { "org" }, -- Or run :TSUpdate org
+        ensure_installed = { "org" },                    -- Or run :TSUpdate org
       }
 
       require("orgmode").setup {
         org_agenda_files = { "~/Library/CloudStorage/GoogleDrive-jjprest@gmail.com/My Drive/orgs/*", "~/my-orgs/**/*" },
         org_default_notes_file = "~/Library/CloudStorage/GoogleDrive-jjprest@gmail.com/My Drive/orgs/refile.org",
-      }
-    end,
-  },
-  {
-    "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-      require("nvim-surround").setup {
-        -- Configuration here, or leave empty to use defaults
       }
     end,
   },
