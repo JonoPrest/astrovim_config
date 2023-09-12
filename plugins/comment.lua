c6b7d13596d2a80862bd5f23b988c1232383ac98
Jono Prest <jjprest@gmail.com>
2023-09-12 12:10:00 +0200

    Correct config for Comment.nvim

--- plugins/comment.lua
+++ plugins/comment.lua
@@ -1,21 +1,22 @@
 return {
   "numToStr/Comment.nvim",
-  keys = {
-    {
-      "gc",
-      mode = { "n", "v" },
-      desc = "Comment toggle linewise",
-    },
-    {
-      "gb",
-      mode = { "n", "v" },
-      desc = "Comment toggle blockwise",
-    },
-  },
-  opts = function()
+  -- keys = {
+  --   {
+  --     "gc",
+  --     mode = { "n", "v" },
+  --     desc = "Comment toggle linewise",
+  --   },
+  --   {
+  --     "gb",
+  --     mode = { "n", "v" },
+  --     desc = "Comment toggle blockwise",
+  --   },
+  -- },
+  config = function()
+    require("Comment").setup()
     local ft = require "Comment.ft"
-
     ft.set("rescript", { "//%s", "/*%s*/" })
+
     local commentstring_avail, commentstring = pcall(require, "ts_context_commentstring.integrations.comment_nvim")
     return commentstring_avail and commentstring and {
       pre_hook = commentstring.create_pre_hook(),
