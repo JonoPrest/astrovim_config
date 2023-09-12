4c91ecd7bb2b614eae2cc8864d4858ce0d356e9b
Jono Prest <jjprest@gmail.com>
2023-09-12 12:09:49 +0200

    add vim-abolish

---
+++ plugins/vim-abolish.lua
@@ -0,0 +1,13 @@
+return {
+  "tpope/vim-abolish",
+  -- config = {
+  --   filesystem = {
+  --     filtered_items = {
+  --       visible = true,
+  --       hide_dotfiles = false,
+  --       hide_gitignored = true,
+  --     },
+  --   },
+  -- },
+  lazy = false,
+}
